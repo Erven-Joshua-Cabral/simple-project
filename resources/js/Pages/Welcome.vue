7dd64c297bc99cc9f2855594e7c41e59222cec0e
Erven-Joshua-Cabral <erven.cabral@awsys-i.com>
2023-10-10 10:46:48 +0800

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,140 @@
+<script setup>
+import {Head, Link} from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Welcome"/>
+
+    <div
+        class="flex flex-col bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+    >
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+            >Dashboard
+            </Link
+            >
+
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Log in
+                </Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Register
+                </Link
+                >
+            </template>
+        </div>
+        <div>
+            <div class="overflow-hidden bg-white">
+                <div class="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+                    <div class="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+                        <div class="sm:max-w-lg">
+                            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Summer styles are
+                                finally here</h1>
+                            <p class="mt-4 text-xl text-gray-500">This year, our new summer collection will shelter you
+                                from the harsh elements of a world that doesn't care if you live or die.</p>
+                        </div>
+                        <div>
+                            <div class="mt-10">
+                                <!-- Decorative image grid -->
+                                <div aria-hidden="true"
+                                     class="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl">
+                                    <div
+                                        class="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                                        <div class="flex items-center space-x-6 lg:space-x-8">
+                                            <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                                <div
+                                                    class="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                            </div>
+                                            <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                            </div>
+                                            <div class="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                                <div class="h-64 w-44 overflow-hidden rounded-lg">
+                                                    <img
+                                                        src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
+                                                        alt="" class="h-full w-full object-cover object-center"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <a href="#"
+                                   class="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">Shop
+                                    Collection</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.bg-dots-darker {
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+
+@media (prefers-color-scheme: dark) {
+    .dark\:bg-dots-lighter {
+        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+    }
+}
+</style>
